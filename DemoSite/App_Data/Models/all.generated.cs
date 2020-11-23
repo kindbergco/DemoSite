97417237d9a2f04b648776e476119073240b89ff
Peter Kindberg <peter@kindbergco.se>
2020-11-23 02:54:12 +0100

    Fixed view of plugin

--- DemoSite/App_Data/Models/all.generated.cs
+++ DemoSite/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "fbfc7eb342ab028d")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.4")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
